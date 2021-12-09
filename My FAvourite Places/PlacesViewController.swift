9ca12937ff894c42e8ac37840c7aff251c64521f
omar <tayeho@outlook.com>
2021-12-09 19:43:55 +0000

    Setup the outlet to the table, and the Ashton building as the default location.

--- My FAvourite Places/PlacesViewController.swift
+++ My FAvourite Places/PlacesViewController.swift
@@ -7,8 +7,19 @@
 
 import UIKit
 
+var places = [[String : String]()]
+
 class PlacesViewController: UITableViewController {
 
+    
+    
+    @IBOutlet var table: UITableView!
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -27,24 +38,47 @@ class PlacesViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 4
-    }
+            // #warning Incomplete implementation, return the number of rows
+            return places.count
+        }
 
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-       
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) ->
+    UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
-        cell.textLabel?.text = "Row \(indexPath.row)"
-        
-        
+        if places[indexPath.row]["name"] != nil {
+            cell.textLabel?.text = places[indexPath.row]["name"]
+        }
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         performSegue(withIdentifier: "toMap", sender: nil)
-    } 
+    }
 
+    override func viewDidAppear(_ animated: Bool) {
+         if places.count == 1 && places[0].count == 0 {
+          places.remove(at: 0)
+          places.append(["name":"Ashton Building", "lat": "53.406566", "lon": "-2.966531"])
+   }
+        
+        table.reloadData()
+      }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
